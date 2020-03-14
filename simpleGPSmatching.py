994b073d344e378be3f3461e3265118a5c16c736
Simon Funke <simon.funke@gmail.com>
2020-03-14 21:13:10 +0100

    Fix datetime and float

--- simpleGPSmatching.py
+++ simpleGPSmatching.py
@@ -1,4 +1,5 @@
 import pandas as pd
-data = pd.read_csv('data1.txt',sep='\t',parse_dates=[1],usecols=[1,2,3])
+df = pd.read_csv('data1.txt', sep='\t', parse_dates=[0], usecols=[1,2,3])
+
 from IPython import embed
 embed()
