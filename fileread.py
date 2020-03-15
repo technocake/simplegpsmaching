b9a7b17cfc84ade169c45c2d845741746eb3b2c4
Simon Funke <simon.funke@gmail.com>
2020-03-15 14:56:18 +0100

    Update fileread.py file

--- fileread.py
+++ fileread.py
@@ -1,38 +1,35 @@
-
 import pandas as pd
 import os
 
 
 def load_trajectories(data_dir="Data/"):
 
-	user_df_map = {}
+    id_counter = 0
+    trajectories = {}
 
- 	# Loop over all data subdirectories
-	for subdir, dirs, files in os.walk(data_dir):
+    # Loop over all data subdirectories
+    for subdir, dirs, files in os.walk(data_dir):
 
-		# Loop over all plt files
-		for file in files:
-			if not file.endswith(".plt"):
-				continue
+        # Loop over all plt files
+        for file in files:
+            if not file.endswith(".plt"):
+                continue
 
-			# Convert plt files to pandas frame
-			df = pd.read_csv(os.path.join(subdir, file), sep=',', 
-				             skiprows=[0,1,2,3,4,5], usecols=[0,1,5,6],
-				             names=["Longitude", "Latitude", "Date", "Time"], header=None)
+            # Convert plt files to Panda frames
+            df = pd.read_csv(os.path.join(subdir, file), sep=',', 
+                             skiprows=[0,1,2,3,4,5], usecols=[0,1,6],
+                             names=["Longitude", "Latitude", "Time"], header=None)
 
-			# Append trahectories to user
-			user = int(subdir[5:8])
-			if user in user_df_map:
-				user_df_map[user] = user_df_map[user].append(df, ignore_index=True)
-			else:
-				user_df_map[user] = df
+            # Attach trahejectories to user
+            trajectories[id_counter] = df
+            id_counter += 1
 
-	return user_df_map
-			
+    return trajectories
+            
 
-user_df_map = load_trajectories()
+trajectories = load_trajectories()
 
 # Get trajectories of first user
-user_df_map[0]
+#trajectories[0]
 
 from IPython import embed; embed()
